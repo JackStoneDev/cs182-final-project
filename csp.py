e8a1b61fd7864ac843ac58aae6b1c06ee6f11a6c
Jack Stone <jack@jackstonedev.com>
2018-12-05 21:27:50 -0500

    Create neighbor function.

--- csp.py
+++ csp.py
@@ -3,7 +3,7 @@ from simulated_annealing import SimulatedAnnealing
 
 data_map = Map(1000)
 
-for i in range(100):
+for i in range(5):
     data_map.add_random_location()
 
 SA = SimulatedAnnealing()
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,9 +1,9 @@
-from random import random
+import random
 import numpy as np
 
 # Simulated annealing class
 class SimulatedAnnealing:
-    max_dist = 10
+    max_distance = 40
 
     # Calculates "cost" of a particular map by taking the average over all locations of minimum distance to another node
     def cost(self, map):
@@ -36,3 +36,48 @@ class SimulatedAnnealing:
 
         # Return average of distances
         return sum / num_coordinates
+
+    # Generates neighboring state of given map by randomly moving locations around until all locations are within an arbitrary distance from one another
+    def neighbor(self, map):
+        # Check if all locations are within an arbitrary distance of another location
+        all_within_distance = False
+
+        while not all_within_distance:
+            all_within_distance = True
+
+            # Swap a random location in the map
+            random_location = random.choice(map.coordinates.keys())
+            del map.coordinates[random_location]
+            map.add_random_location()
+
+            # Get all coordinates of map
+            coordinates = map.coordinates.keys()
+
+            # Loop through coordinates
+            for location in coordinates:
+                # Initialize minimum distance
+                min_distance = float('inf')
+
+                # Track if the location is within an arbitrary distance from any other node
+                location_within_distance = False
+
+                # Loop through neighbors
+                for neighbor in coordinates:
+                    # If the neighbor is the same point, then skip
+                    if location == neighbor:
+                        continue
+
+                    # Calculate Euclidean distance to neighbor
+                    distance = np.linalg.norm(np.subtract(location, neighbor))
+
+                    # Is this distance within the arbitrary distance?
+                    if distance <= self.max_distance:
+                        location_within_distance = True
+                        break
+
+                # If this location is not within the distance, then we must make another swap in the graph
+                if not location_within_distance:
+                    all_within_distance = False
+                    break
+
+        return map
