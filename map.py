c24c415bb942de72e8247c54ef21c52a165cfb92
Jack Stone <jack@jackstonedev.com>
2018-12-05 20:38:45 -0500

    Define max population density and take in map size.

--- map.py
+++ map.py
@@ -1,9 +1,14 @@
 # Map class
 class Map:
+    # Constant for maximum population density allowed
+    max_population_density = 100000
+
     # Constructor
-    def __init__(self):
+    # Accepts size of N x N map
+    def __init__(self, map_size):
         # Initialize coordinates to empty dictionary
         self.coordinates = {}
+        self.map_size = map_size
 
     # Adds location to the map
     # Takes x, y coordinates, boolean representing road or not, and population density as number
