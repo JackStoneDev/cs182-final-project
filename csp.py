93d0e6f168e0550520aa79a67841f146ca5a6fa6
Jack Stone <jack@jackstonedev.com>
2018-12-05 20:05:09 -0500

    Create add coordinate function for map.

--- csp.py
+++ csp.py
@@ -1,3 +1,5 @@
 from map import Map
 
 data_map = Map()
+data_map.add((1, 2), True, 1000)
+data_map.add((3, 4), False, 9999.9)
--- map.py
+++ map.py
@@ -4,3 +4,11 @@ class Map:
     def __init__(self):
         # Initialize coordinates to empty dictionary
         self.coordinates = {}
+
+    # Adds location to the map
+    # Takes x, y coordinates, boolean representing road or not, and population density as number
+    def add(self, coordinates, road, population_density):
+        self.coordinates[coordinates] = {
+            'road': road,
+            'population_density': population_density
+        }
