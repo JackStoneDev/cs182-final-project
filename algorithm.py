825360d208bcae75f77d77235898b6b509f60469
Jack Stone <jack@jackstonedev.com>
2018-12-16 22:31:46 -0500

    Add comments.

--- algorithm.py
+++ algorithm.py
@@ -3,21 +3,26 @@ from simulated_annealing import SimulatedAnnealing
 from plot import Plot
 from k_means import KMeans
 
+# Generate random map
 data_map = Map(25)
 
+# Add 20 random city points
 for i in range(20):
     data_map.add_random_location()
 
+# Add 5 random drop zones
 for i in range(5):
     data_map.add_random_dropoff_zone()
 
+# Show initial map cost
 SA_final = SimulatedAnnealing(3)
 print 'Initial map cost: %d' % SA_final.cost(data_map)
 
-# Plot old map
+# Plot initial map
 plot = Plot()
 plot.plot(data_map, False)
 
+# Run simulated annealing experiments
 for i in range(1, 5):
     print 'Running simulated annealing for type %d:' % i
 
@@ -27,6 +32,7 @@ for i in range(1, 5):
     print 'Cost: %d' % SA_final.cost(new_map)
     plot.plot(new_map, False, i)
 
+# Run k-means
 dropoff_zones = data_map.dropoff_zones.keys()
 K = len(dropoff_zones)
 locations = data_map.coordinates.keys()
